303d14da3f9a6ee34cb1cd3fb19982e69a7922d3
Sergei Mishin <SMishinMail@gmail.com>
2023-06-21 09:45:42 +0700

    Adds tests for routes

---
+++ news/tests/test_trial.py
@@ -0,0 +1,34 @@
+# news/tests/test_trial.py
+from unittest import skip
+
+from django.test import TestCase
+
+# Импортируем модель, чтобы работать с ней в тестах.
+from news.models import News
+
+
+# @skip
+# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
+class TestNews(TestCase):
+
+    # В методе класса setUpTestData создаём тестовые объекты.
+    # Оборачиваем метод соответствующим декоратором.
+    @classmethod
+    def setUpTestData(cls):
+        # Стандартным методом Django ORM create() создаём объект класса.
+        # Присваиваем объект атрибуту класса: назовём его news.
+        cls.news = News.objects.create(
+            title='Заголовок новости',
+            text='Тестовый текст',
+        )
+
+    # Проверим, что объект действительно было создан.
+    def test_successful_creation(self):
+        # При помощи обычного ORM-метода посчитаем количество записей в базе.
+        news_count = News.objects.count()
+        # Сравним полученное число с единицей.
+        self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        # Сравним свойство объекта и ожидаемое значение.
+        self.assertEqual(self.news.title, 'Заголовок новости')
